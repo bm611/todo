79dd8f0e3756d218f04a10efdc2dcdf4574d3583
Bharath Mohan <broxdeez@Bharaths-MacBook-Pro.local>
2024-08-19 11:08:11 -0700

    added input box for adding and searching todo

--- todo/todo.py
+++ todo/todo.py
@@ -38,15 +38,15 @@ def index() -> rx.Component:
         rx.vstack(
             rx.color_mode.button(position="top-right"),
             # rx.box("Daisy UI", class_name="btn btn-primary btn-outline"),
-            rx.heading("Add Todo", class_name="mt-20"),
-            rx.input(
-                placeholder="Add a new to-do...",
-                class_name="p-4 w-3/4 h-20 rounded-2xl text-2xl",
-            ),
             rx.heading("Search Todo", class_name="mt-20"),
             rx.input(
                 placeholder="Enter your search term...",
-                class_name="p-4 w-3/4 h-20 rounded-2xl text-2xl",
+                class_name="p-4 w-3/4 h-20 rounded-2xl text-lg md:text-2xl",
+            ),
+            rx.heading("Add Todo", class_name="mt-20"),
+            rx.input(
+                placeholder="Add a new to-do...",
+                class_name="p-4 w-3/4 h-20 rounded-2xl text-lg md:text-2xl",
             ),
             rx.logo(
                 class_name="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-4"
