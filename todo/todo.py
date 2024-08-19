691a6d1b0563ad15f7ce13f9e21f6a66f13c9a6c
Bharath Mohan <broxdeez@Bharaths-MBP.attlocal.net>
2024-08-19 14:23:32 -0700

    todo block UI & logic for adding todo

--- todo/todo.py
+++ todo/todo.py
@@ -1,10 +1,24 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
+from todo.components.todo_block import todo_block
 
 
 class State(rx.State):
     """The app state."""
+
+    todo_list = ["apple", "orange"]
+    new_todo = ""
+
+    def set_new_todo(self, value: str):
+        self.new_todo = value
+
+    def add_todo(self, todo_item: str):
+        self.todo_list.append(todo_item)
+
+    def remove_todo(self, index: int):
+        if 0 <= index < len(self.todo_list):
+            self.todo_list.pop(index)
 
 
 # def action_bar() -> rx.Component:
@@ -44,10 +58,16 @@ def index() -> rx.Component:
                 class_name="p-4 w-3/4 h-20 rounded-2xl text-lg md:text-2xl",
             ),
             rx.heading("Add Todo", class_name="mt-20"),
-            rx.input(
-                placeholder="Add a new to-do...",
-                class_name="p-4 w-3/4 h-20 rounded-2xl text-lg md:text-2xl",
+            rx.form(
+                rx.input(
+                    placeholder="Add a new to-do...",
+                    class_name="mb-10 p-4 w-3/4 h-20 rounded-2xl text-lg md:text-2xl mx-auto",
+                    value=State.new_todo,
+                    on_change=State.set_new_todo,
+                ),
+                enter_key_submit=True,
             ),
+            rx.foreach(State.todo_list, todo_block),
             rx.logo(
                 class_name="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-4"
             ),
